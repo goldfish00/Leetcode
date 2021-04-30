f325a716a30d50253e4d7fb4bd8c8f0815815bdf
Junyi Huang <junyih@umich.edu>
2021-04-30 21:49:36 +0800

    upto 165

--- code/part2.cpp
+++ code/part2.cpp
@@ -707,28 +707,523 @@ public:
 
 /* #153 *//* Binary search: find the first element smaller than both its neighbors
            To locate the interval where the target is: start of the interval id greater than the end of the iinterval
-           [left, middle], (middle + 1, right]
-           */
+           [left, middle], [middle + 1, right]
+           *//*
 class Solution {
 public:
     int findMin(vector<int>& nums) {
         if (nums.size() == 1) return nums.back();
         int left = 0;
         int right = int(nums.size() - 1);
-        int middle = left + (right - left) / 2;
-        if (nums[left] <= nums[middle] && nums[middle + 1] <= nums[right]) return nums[middle + 1];
+        while (right - left > 1) {
+            if (nums[left] < nums[right]) return nums[left];
+            int middle = left + (right - left) / 2;
+            if (nums[left] > nums[middle]){
+                right = middle;
+            }
+            else if (nums[middle] > nums[right]){
+                left = middle;
+            }
+        }
+        return min(nums[left], nums[right]);
+    }
+};*/
+
+
+/* #154 *//*
+class Solution {
+    int recur(vector<int>& nums, int left, int right){
+        while (right - left > 1) {
+            if (nums[left] < nums[right]) return nums[left];
+            int middle = left + (right - left) / 2;
+            if (nums[left] > nums[middle]){
+                right = middle;
+            }
+            else if (nums[middle] > nums[right]){
+                left = middle;
+            }
+           // the solution optimizes this step by decreasing right by 1 as an update
+            else if (nums[middle] == nums[right] && nums[right] == nums[left]){
+                return min(recur(nums, left, middle), recur(nums, middle, right));
+            }
+        }
+        return min(nums[left], nums[right]);
+    }
+    
+public:
+    int findMin(vector<int>& nums) {
+        if (nums.size() == 1) return nums.back();
+        return recur(nums, 0, int(nums.size() - 1));
+    }
+};*/
+
+
+/* #155 *//* This solutioin is inefficient, can use a pair to memorize the minimum number before each element, with the
+           element itself inclusive *//*
+class MinStack {
+public:
+    deque<int> vals;
+    multiset<int> sortedVals;
+    
+    MinStack(){}
+    
+    void push(int val) {
+        vals.push_back(val);
+        sortedVals.insert(val);
+    }
+    
+    void pop() {
+        sortedVals.erase(sortedVals.find(vals.back()));
+        vals.pop_back();
+    }
+    
+    int top() {
+        return vals.back();
+    }
+    
+    int getMin() {
+        return *sortedVals.begin();
+    }
+};*/
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+/* #156 *//*
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {
+        if (!root) return root;
+        if (!root->left && !root->right) return root;
+        if (!root->right && !root->left->left){
+            TreeNode* newRoot = root->left;
+            newRoot->right = root;
+            root->left = nullptr;
+            return newRoot;
+        }
+        if (!root->left->left && !root->right->right){
+            TreeNode* newRoot = root->left;
+            newRoot->left = root->right;
+            newRoot->right = root;
+            root->left = nullptr;
+            root->right = nullptr;
+            return newRoot;
+        }
+        
+        TreeNode* newRoot = upsideDownBinaryTree(root->left);
+        root->left->left = root->right;
+        root->left->right = root;
+        root->left = nullptr;
+        root->right = nullptr;
+        return newRoot;
+    }
+};*/
+
+/* #157 *//*
+extern int read4(char *buf4);
+class Solution {
+public:
+    int read(char *buf, int n) {
+        int timesRead = n / 4;
+        int lastRead = n % 4;
+        int bytesRead = 0;
+        for (int i = 0; i < timesRead; ++i) {
+            int numRead = read4(buf + bytesRead);
+            if (numRead != 4){
+                bytesRead += numRead;
+                return bytesRead;
+            }
+            bytesRead += 4;
+        }
+        int numRead = read4(buf + bytesRead);
+        bytesRead += numRead;
+        return min(bytesRead, n);
+    }
+};*/
+
+
+/* #158 *//*
+extern int read4(char *buf4);
+class Solution {
+public:
+    char* buffBegin = buff;
+    int numLeftOver = 0;
+    char buff[4];
+    
+    int read(char *buf, int n) {
+        if (n <= numLeftOver){
+            for (int i = 0; i < n; ++i) {
+                *buf = *buffBegin;
+                ++buf;
+                ++buffBegin;
+                --numLeftOver;
+            }
+            if (numLeftOver == 0) buffBegin = buff;
+            return n;
+        }
+        // if greater, read the numLeftOver first
+        int bytesRead = 0;
+        for (int i = 0; i < numLeftOver; ++i) {
+            *(buf + i) = *buffBegin;
+            ++buffBegin;
+        }
+        buffBegin = buff;
+        bytesRead += numLeftOver;
+        n -= numLeftOver;
+        numLeftOver = 0;
+
+        int timesRead = n / 4;
+        for (int i = 0; i < timesRead; ++i) {
+            int numRead = read4(buf + bytesRead);
+            if (numRead != 4){
+                bytesRead += numRead;
+                return bytesRead;
+            }
+            bytesRead += 4;
+        }
+        int numRead = read4(buff);
+        int numLastRead = min(numRead, n % 4);
+        numLeftOver = numRead - numLastRead;
+        
+        for (int i = 0; i < numLastRead; ++i) {
+            *(buf + bytesRead) = *buffBegin;
+            ++buffBegin;
+            ++bytesRead;
+        }
+        
+        return bytesRead;
+    }
+};*/
+
+
+/* #159 *//* Sliding window *//*
+class Solution {
+private:
+    int maxSubLen = 1;
+    unordered_map<char, int> distinctChars;
+    
+    bool exist(char c){
+        return distinctChars.find(c) != distinctChars.end();
+    }
+    
+    void erase(char c){
+        auto it = distinctChars.find(c);
+        if (it->second == 1) distinctChars.erase(it);
+        else it->second -= 1;
+    }
+    
+    size_t size(){
+        return distinctChars.size();
+    }
+    
+    void insert(char c){
+        distinctChars[c] += 1;
+    }
+    
+    
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        if (s.length() < 3) return int(s.length());
+        // find the first window with two distinct chars
+        size_t start = 0;
+        size_t end = 0;
+        insert(s[end]);
+        // start sliding
+        while (end < s.length()) {
+            if (size() < 2){
+                maxSubLen = max(maxSubLen, int(end - start + 1));
+                ++end;
+                insert(s[end]);
+            }
+            else if(size() == 2) {
+                maxSubLen = max(maxSubLen, int(end - start + 1));
+                ++end;
+                insert(s[end]);
+            }
+            // if greater than 2
+            else {
+                erase(s[start]);
+                ++start;
+            }
+        }
+        return maxSubLen;
+    }
+};*/
+
+
+/* #160 *//*
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+private:
+    int getSize(ListNode* head){
+        int size = 0;
+        while (head) {
+            head = head->next;
+            ++size;
+        }
+        return size;
+    }
+    
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int sizeA = getSize(headA);
+        int sizeB = getSize(headB);
+        // set headA to be the one of greater size
+        if (sizeA < sizeB) {
+            swap(headA, headB);
+            swap(sizeA, sizeB);
+        }
+        for (int i = 0; i < sizeA - sizeB; ++i) {
+            headA = headA->next;
+        }
+        while (headA) {
+            if (headB == headA) return headB;
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return nullptr;
+    }
+};*/
+
+
+/* #161 *//*
+class Solution {
+public:
+    bool isOneEditDistance(string s, string t) {
+        if (s.length() > t.length() + 1) return false;
+        if (t.length() > s.length() + 1) return false;
+        
+        // case 1: the strings are of the same length and only replacing operation is considered
+        if (s.length() == t.length()){
+            bool oneDifference = false;
+            for (size_t i = 0; i < s.length(); ++i){
+                if (s[i] != t[i]){
+                    if (oneDifference) return false;
+                    else oneDifference = true;
+                }
+            }
+            return oneDifference;
+        }
+        // case 2: two strings differ by 1 in size and consider only inserting
+        // set s to be the longer string
+        if (s.length() < t.length()) swap(s, t);
+        bool oneDifference = false;
+        for (size_t i = 0; i < s.length(); ++i) {
+            if (oneDifference) {
+                if (s[i] != t[i - 1]) return false;
+            }
+            else {
+                if (s[i] != t[i]) oneDifference = true;
+            }
+        }
+        return true;
+    }
+};*/
+
+
+/* #162 *//*
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size() == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        bool greaterThanRight = false;
+        for (int i = 1; i + 1 < nums.size(); ++i) {
+            if (greaterThanRight) {
+                greaterThanRight = (nums[i] <= nums[i + 1]);
+                continue;
+            }
+            else{
+                if (nums[i] > nums[i + 1]) return i;
+                else greaterThanRight = false;
+            }
+        }
+        return int(nums.size() - 1);
+    }
+};*/
+
+
+/* #163 *//*
+class Solution {
+private:
+    vector<string> results;
+    string arrow;
+    
+    void genResult(int low, int high){
+        if (low > high) return;
+        if (low == high) results.push_back(to_string(low));
+        else results.push_back(to_string(low) + arrow + to_string(high));
+    }
+    
+public:
+    vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        arrow = "->";
+        // check the beginning
+        if (nums.size() == 0){
+            genResult(lower, upper);
+            return results;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i == 0){
+                genResult(lower, nums[i] - 1);
+            }
+            else {
+                if (nums[i - 1] + 1 != nums[i]) genResult(nums[i - 1] + 1, nums[i] - 1);
+            }
+            // add the last one
+            if (i == nums.size() - 1) genResult(nums.back() + 1, upper);
+        }
+        return results;
+    }
+};*/
+
+
+/* #164 *//*
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if (nums.size() == 1) return 0;
+        sort(nums.begin(), nums.end());
+        int maxGap = INT_MIN;
+        for (int i = 1; i < nums.size(); ++i) {
+            maxGap = max(maxGap, nums[i] - nums[i - 1]);
+        }
+        return maxGap;
+    }
+};*/
+
+
+/* #165 *//* There are some bad coding styles with the two private member functions (redundancy) *//*
+class Solution {
+private:
+    char dot = '.';
+    
+    int cmpSubstring(string& version1, string& version2, size_t& ind1, size_t& ind2){
+        size_t ind1End = version1.find(dot, ind1);
+        size_t ind2End = version2.find(dot, ind2);
+        if (ind1End == string::npos) ind1End = version1.size();
+        if (ind2End == string::npos) ind2End = version2.size();
+        int ver1 = stoi(version1.substr(ind1, ind1End - ind1));
+        int ver2 = stoi(version2.substr(ind2, ind2End - ind2));
+        ind1 = min(ind1End + 1, version1.size());
+        ind2 = min(ind2End + 1, version2.size());
+        if (ver1 > ver2) return 1;
+        else if (ver1 == ver2) return 0;
+        else return -1;
+    }
+    
+    bool isZero(string& version, size_t &ind){
+        size_t indEnd = version.find(dot, ind);
+        if (indEnd == string::npos) indEnd = version.size();
+        int val = stoi(version.substr(ind, indEnd - ind));
+        ind = min(indEnd + 1, version.size());
+        return val == 0;
+    }
+    
+public:
+    int compareVersion(string version1, string version2) {
+        size_t ind1 = 0;
+        size_t ind2 = 0;
+        while (ind1 < version1.size() && ind2 < version2.size()) {
+            int cmpRes = cmpSubstring(version1, version2, ind1, ind2);
+            if (cmpRes != 0) return cmpRes;
+        }
+        
+        if (ind1 == version1.size() && ind2 == version2.size()) return 0;
+        if (ind1 < version1.size()){
+            while (ind1 < version1.size()) {
+                if(!isZero(version1, ind1)) return 1;
+            }
+            return 0;
+        }
+        else{
+            while (ind2 < version2.size()) {
+                if (!isZero(version2, ind2)) return -1;
+            }
+            return 0;
+        }
+    }
+};*/
+
+/* #166 */
+class Solution {
+private:
+    int maxBound9 = INT_MAX / 9;
+    int maxBound10 = INT_MAX / 10;
+    int times = 0;
+    int num9 = 0;
+    int num10 = 0;
+    
+    bool base10(int denominator){
+        while (denominator % 10 == 0) {
+            denominator /= 10;
+            ++num10;
+        }
+        while (denominator % 2 == 0) {
+            denominator /= 2;
+        }
+        while (denominator % 5 == 0) {
+            denominator /= 5;
+        }
+        return denominator == 1 || denominator == -1;
+    }
+    
+    void base9(int denominator){
+        int denominator9 = 9;
+        ++num9;
+        while (denominator9 % denominator != 0) {
+            denominator9 = denominator9 * 10 + 9;
+            ++num9;
+        }
+        times = denominator9 / denominator;
+    }
+    
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        int integerPart = numerator / denominator;
+        if (numerator % denominator == 0) return to_string(integerPart);
+        bool negative = false;
+        if ((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0)) {
+            negative = true;
+            integerPart += 1;
+        }
+        if (base10(denominator)) return to_string(double(numerator / denominator));
+        // deal with the cycle case
+        numerator -= denominator * integerPart;
+        
+        string result(to_string(integerPart));
+        result.push_back('.');
         
     }
 };
 
 
 int main(){
-    //Solution S;
+    Solution S;
     //vector<string> words{"zzyy","zy","zyy"};
     //vector<vector<char>> board{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
     vector<int> nums{94,89,54,26,54,54,99,64};
     vector<string> words{"cat","cats","and","sand","dog"};
-    
     return 0;
 }
 
