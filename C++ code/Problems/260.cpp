46b62b36a628c5344d216c3f864c70e44850844f
Junyi Huang <junyih@umich.edu>
2023-03-10 19:33:27 +0800

    daily practice

--- C++ code/Problems/260.cpp
+++ C++ code/Problems/260.cpp
@@ -14,6 +14,29 @@ using namespace std;
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (const int& num : nums) {
+            res ^= num;
+        }
         
+        // get the first bit where x and y differs
+        int diff = (res == INT_MIN) ? 1 : res & (- res);
+        int x = 0, y = 0;
+        for (const int& num : nums) {
+            if ((num & diff) != 0) {
+                x ^= num;
+            } else {
+                y ^= num;
+            }
+        }
+        return vector<int>{x, y};
     }
 };
+
+int main() {
+    Solution S;
+    vector<int> nums{1,2,1,3,2,5};
+    S.singleNumber(nums);
+    
+    return 0;
+}
